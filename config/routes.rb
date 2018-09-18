856472203cf044e1b06fb7346c894d2bb409bd06
山田秀明 <yamadahideaki@yamadahideakinoMacBook-Pro.local>
2018-09-18 15:57:33 +0900

    [finish]5-6

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ resources :post_images, only: [:new, :create, :index, :show, :destroy] do
 	resource :favorites, only: [:create, :destroy]
     resource :post_comments, only: [:create, :destroy]
 end
-    resources :users, only: [:show]
+    resources :users, only: [:show, :edit, :update]
 
 
 end
